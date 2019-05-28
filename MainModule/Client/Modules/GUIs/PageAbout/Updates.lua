cbe63766ae6a4e0ce51ce882d0893c5478bbe417
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-28 15:43:21 +0100

    Create Updates.lua

---
+++ MainModule/Client/Modules/GUIs/PageAbout/Updates.lua
@@ -0,0 +1,21 @@
+local updates = {
+	
+	{
+	{1546851900, "14/01/2019"};
+	{"!laserEyes command (Donor)"};
+	{"!thanos command (Donor)"};
+	{"!headSnap command (Donor)"};
+	{"!fart command (Donor)"};
+	{"!warp command (Donor)"};
+	};
+	
+	{
+	{1546851900, "13/01/2019"};
+	{"Redesigned UI"};
+	};
+	
+}
+
+
+
+return updates
