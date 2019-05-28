c3f5335b3f6adc8a27b0e411f7a59865390b7edb
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-28 15:43:04 +0100

    Create Credits.lua

---
+++ MainModule/Client/Modules/GUIs/PageAbout/Credits.lua
@@ -0,0 +1,20 @@
+return {
+	
+	{
+	{4,"Project Lead"};
+	{"ForeverHD", {"Core Programming", "UI Design", "Concept Creation"}};
+	};
+	
+	{
+	{5.5, "Top Contributors"};
+	{"Alvin_Blox", {"HD Admin Tutorials", "YouTube: AlvinBlox"}};
+	{"GigsD4X", {"F3X Building Tools"}};
+	};
+		
+	{
+	{6, "Contributors"};
+	{"ZapSplat", {"Sound Effects"}};
+	{"Super_Block", {"Icecream Truck"}};
+	};
+	
+}
