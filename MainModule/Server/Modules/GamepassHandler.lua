0ae255f9e7038c3b357e5f99cfecf06acf364879
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-28 17:12:26 +0100

    Create GamepassHandler.lua

---
+++ MainModule/Server/Modules/GamepassHandler.lua
@@ -0,0 +1,26 @@
+local module = {}
+
+
+local main = require(game:GetService("ReplicatedStorage").HDAdminContainer.SharedModules.MainFramework) local modules = main.modules
+
+
+--Purchase and receive gamepasses
+main.marketplaceService.PromptGamePassPurchaseFinished:Connect(function(player,assetId,isPurchased)
+	local pdata = main.pd[player]
+	if isPurchased and pdata then
+		local validGamepass = main.permissions.gamepasses[tostring(assetId)]
+		if validGamepass and not modules.cf:FindValue(pdata.Gamepasses, assetId) then
+			modules.PlayerData:InsertStat(player, "Gamepasses", assetId)
+			modules.cf:RankPlayerSimple(player, validGamepass.Rank)
+			if not modules.cf:FindValue(main.products, assetId) then
+				modules.cf:FormatAndFireNotice(player, "UnlockRank", modules.cf:GetRankName(validGamepass.Rank))
+			end
+			modules.cf:CheckAndRankToDonor(player, pdata, assetId)
+		end
+	end
+end)
+
+
+
+
+return module
