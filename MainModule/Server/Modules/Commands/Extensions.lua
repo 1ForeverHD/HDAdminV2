8bf1c380796273484466f8ff234902ff18a1a7f1
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-28 16:31:59 +0100

    Create Extensions.lua

---
+++ MainModule/Server/Modules/Commands/Extensions.lua
@@ -0,0 +1,64 @@
+-- This is for commands which continue on respawn
+local module = {}
+
+
+-- << RETRIEVE FRAMEWORK >>
+local main = require(game:GetService("ReplicatedStorage").HDAdminContainer.SharedModules.MainFramework)
+local modules = main.modules
+local settings = main.settings
+
+
+
+-- << SETUP ITEMS (items which continue on respawn) >>
+function module:SetupItem(player, itemName, playerRespawned)
+	if playerRespawned then
+		wait(0.1)
+	end
+	local pdata = main.pd[player]
+	local item = pdata.Items[itemName]
+	local head = modules.cf:GetHead(player)
+	if head and pdata then
+		
+		if itemName == "FreezeBlock" then
+			modules.cf:Movement(false, player)
+			modules.cf:SetTransparency(player.Character, 1)
+			if item:FindFirstChild("FreezeClone") then
+				main.network.ChangeCameraSubject:FireClient(player, (item.FreezeClone.Humanoid))
+			end
+			
+		elseif itemName == "JailCell" then
+			head.CFrame = item.Union.CFrame
+		
+		elseif itemName == "ControlPlr" then
+			local plr = item.Value
+			local controllerHumanoid = modules.cf:GetHumanoid(player)
+			if plr and controllerHumanoid then
+				modules.MorphHandler:BecomeTargetPlayer(player, plr.UserId)
+				main.network.ChangeCameraSubject:FireClient(player, (controllerHumanoid))
+				modules.cf:CreateFakeName(player, plr.Name)
+			else
+				modules.cf:RemoveControlPlr(player)
+			end
+			
+		elseif itemName == "UnderControl" then
+			local controller = item.Value
+			local controllerHumanoid = modules.cf:GetHumanoid(controller)
+			if controller and controllerHumanoid then
+				main.network.ChangeCameraSubject:FireClient(player, (controllerHumanoid))
+				--modules.cf:SetTransparency(player.Character, 1, true)
+				--modules.cf:Movement(false, player)
+				player.Character.Parent = nil
+			else
+				modules.cf:RemoveUnderControl(player)
+			end
+			
+			-- Controler
+			-- Plr
+			
+		end
+	end
+end
+
+
+
+return module
