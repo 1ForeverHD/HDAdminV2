f61d9d0d8e72961436d482f528ea5b3a378b5c2c
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-28 15:35:42 +0100

    Create ClientAPI.lua

---
+++ MainModule/Client/Modules/ClientAPI.lua
@@ -0,0 +1,31 @@
+local hd = {}
+
+
+-- << RETRIEVE FRAMEWORK >>
+local main = require(game:GetService("ReplicatedStorage").HDAdminContainer.SharedModules.MainFramework) local modules = main.modules
+
+
+
+-- << VARIABLES >>
+local topBarFrame = main.gui.CustomTopBar
+
+
+
+-- << API >>
+function hd:SetTopbarTransparency(number)
+	number = tonumber(number) or 0.5
+	topBarFrame.BackgroundTransparency = number
+end
+
+function hd:SetTopbarEnabled(boolean)
+	if type(boolean) ~= "boolean" then
+		boolean = true
+	end
+	main.topbarEnabled = boolean
+	topBarFrame.Visible = boolean
+	modules.TopBar:CoreGUIsChanged()
+end
+
+
+
+return hd
