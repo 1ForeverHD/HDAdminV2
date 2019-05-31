1c8eabb7f331e87b5f4c3c4b6914cdd1be78b44b
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-31 12:06:40 +0100

    Update PageCommands.lua

--- MainModule/Client/Modules/GUIs/PageCommands.lua
+++ MainModule/Client/Modules/GUIs/PageCommands.lua
@@ -2,8 +2,8 @@ local module = {}
 
 
 -- << RETRIEVE FRAMEWORK >>
-local main = require(game:GetService("ReplicatedStorage").HDAdminContainer.SharedModules.MainFramework) local modules = main.modules
-
+local main = _G.HDAdminMain
+local modules = main.modules
 
 
 -- << VARIABLES >>
