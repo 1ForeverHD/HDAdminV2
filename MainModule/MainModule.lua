7332856a8cdb9508d89a472dcdb79de34b1ebe50
Ben H <51117782+ForeverhdRBX@users.noreply.github.com>
2019-05-31 12:00:52 +0100

    Update MainModule.lua

--- MainModule/MainModule.lua
+++ MainModule/MainModule.lua
@@ -1,36 +1,8 @@
 local module = {}
 
--- Main Setup
-function module:Initialize(loaderFolder)
+function module:Initialize(loader)
 	local main = require(script.Client.SharedModules.MainFramework)
-	for a,b in pairs(loaderFolder:GetChildren()) do
-		if b:IsA("ModuleScript") and b.Name == "Settings" then
-			main.settings = require(b)
-		end
-	end
-	main:Initialize("Server", script)
-	main.modules.LoaderHandler:Setup(loaderFolder)
+	main:Initialize("Server", loader)
 end
-
-
-
--- Old Setup (will remain for a few weeks to enable developers to update their Loaders)
-function module:UpdateSettings(oldSettings)
-	local main = require(script.Client.SharedModules.MainFramework)
-	local loaderFolder = script.Server.Assets.LoaderCopy
-	for a,b in pairs(loaderFolder:GetChildren()) do
-		if b:IsA("ModuleScript") and b.Name == "Settings" then
-			main.settings = require(b)
-		end
-	end
-	for a,b in pairs(oldSettings) do
-		main.settings[a] = b
-	end
-	main.oldLoader = true
-	main:Initialize("Server", script)
-	main.modules.LoaderHandler:Setup(loaderFolder)
-end
-
-
 
 return module
